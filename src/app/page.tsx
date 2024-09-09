16a351d7add28b23991350c9160eeca9b3c417ce
Nam Tinnakorn RMU <tinnakorn.pei@rmu.ac.th>
2024-09-10 00:31:03 +0700

    set type state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { loginAction, showAction } from "./action";
 import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [show, setShow] = useState<string>();
+  const [show, setShow] = useState<string>("");
   useEffect(() => {
     showAction().then((data) => setShow(data));
   }, []);
